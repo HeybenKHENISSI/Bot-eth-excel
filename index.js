a0f8387ab4537a7f0f2aeaaea763167502357cb0
HeybenKHENISSI <83506871+HeybenKHENISSI@users.noreply.github.com>
2022-01-01 23:28:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,9 +47,11 @@ client.on("message", async message => {
 
     if (message.author.bot == false) {
         if (message.author.username.includes("Announcement") || message.author.username.includes("Admin") || message.author.username.includes("Moderator") || message.author.username.includes("Important") || message.author.username.includes("Mint") || message.author.username.includes("Minting") || message.author.username.includes("Administrator") || message.author.username.includes("Director") || message.author.username.includes("CEO") || message.author.username.includes("CTO") || message.author.username.includes("Manager") || message.author.username.includes("Announcements") || message.author.username.includes("Bot") || message.author.username.includes("Notification") || message.author.username.includes("Info") || message.author.username.includes("reminder") || message.author.username.includes("Annoucement") || message.author.username.includes("Information") || message.author.username.includes("Informations") || message.author.username.includes("annoucement") || message.author.username.includes("verification") || message.author.username.includes("annoucment") || message.author.username.includes("Annoucment") || message.author.username.includes("verification") || message.author.username.includes("Verification") || message.author.username.includes("mint") || message.author.username.includes("news") || message.author.username.includes("giveaway") || message.author.username.includes("giveaways") ||  message.author.username.includes("Presale") ||  message.author.username.includes("presale") ||  message.author.username.includes("PRESALE")) {
+            if (message.author.bot == false) {
             //console.log(/*"est ban"*/"est" + member.user.username)
             console.log(message.author.username + " est ban car il possède un pseudon non conforme")
             message.guild.member(message.author.id).ban({ reason: 'ban' })
+            }
         }
     }
 
